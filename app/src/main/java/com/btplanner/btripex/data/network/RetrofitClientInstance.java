2a155cb3a020af63b631e44f21d181e2b765283a
GioXmen <ernestas1@yandex.ru>
2020-04-18 02:49:34 +0300

    Increase timeout for rest calls, fix scaling issue

--- app/src/main/java/com/btplanner/btripex/data/network/RetrofitClientInstance.java
+++ app/src/main/java/com/btplanner/btripex/data/network/RetrofitClientInstance.java
@@ -1,5 +1,11 @@
 package com.btplanner.btripex.data.network;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,9 +16,19 @@ public class RetrofitClientInstance {
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+            Gson gson = new GsonBuilder()
+                    .setLenient()
+                    .create();
+            OkHttpClient okhttpclient = new OkHttpClient.Builder()
+                    .connectTimeout(10, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(15, TimeUnit.SECONDS)
+                    .build();
+
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .callFactory(okhttpclient)
                     .build();
         }
         return retrofit;
